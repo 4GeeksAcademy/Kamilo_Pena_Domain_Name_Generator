329d81306548916e2546f1de5f7b19a37f9c44f8
KmiloPena <125616941+KmiloPena@users.noreply.github.com>
2023-10-04 01:15:43 +0000

    Domain_Name_Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "their", "her"];
+  let adj = ["great", "big", "wonderful", "magic"];
+  let noun = ["jogger", "racoon", "cat", "dog"];
+  let hacks = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    let randomPronoun = pronoun[i];
+    for (let a = 0; a < adj.length; a++) {
+      let randomAdj = adj[a];
+      for (let b = 0; b < noun.length; b++) {
+        let randomNoun = noun[b];
+        for (let c = 0; c < hacks.length; c++) {
+          let randomHacks = hacks[i];
+          console.log(randomPronoun, randomAdj, randomNoun, randomHacks);
+        }
+      }
+    }
+  }
 };
